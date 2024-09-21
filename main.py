564e530733b43f04c0201115d6428f15d1dc0fcd
Massimo Lopez <mlopez9876@gmail.com>
2024-09-20 22:05:58 -0400

    📦 NEW: CB-7 add toggle, updated extensions

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ st.write(
 # Welcome to the Family Faces app!"""
 )
 
+in_zipfile_mode = st.toggle(label="Upload a Zip folder with photos instead of a set of photos.")
 
-images = st.file_uploader(
-    label="Please upload a zip folder containing your images",
-    type=["PNG", "JPEG", "JPG"],
-    accept_multiple_files=True
-)
+if in_zipfile_mode:
+    images = st.file_uploader(
+        label="Please upload a zip folder containing your images",
+        type=["ZIP"],
+    )
+else:
+    images = st.file_uploader(
+        label="Please upload a zip folder containing your images",
+        type=["PNG", "JPEG", "JPG"],
+    )
+
+ai_button_pressed = st.button(label="Start Image Recognition")
