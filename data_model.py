8f5077937ac44bca74e246da401e6350ef2b055e
Massimo Lopez <mlopez9876@gmail.com>
2024-09-25 01:12:59 -0400

    📦 NEW: CB-6 Tokenize images

--- data_model.py
+++ data_model.py
@@ -4,6 +4,9 @@ import glob
 
 import pandas as pd
 
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+
 
 class DataModels:
     """Class for dataframes and model manipulation"""
@@ -13,3 +16,20 @@ class DataModels:
         self.photo_df = pd.DataFrame.from_records(
             [glob.glob("images/**/*.*", recursive=True)]
         ).T
+
+        self.tokenize()
+
+    def tokenize(self) -> None:
+        """Tokenize the images"""
+        # Initialize Img2Vec with GPU
+        img2vec = Img2Vec(cuda=True)
+
+        # Open the images
+        images_list = [Image.open(name[0]) for name in self.photo_df.values]
+
+        # Create the vector
+        self.image_vectors = pd.DataFrame(img2vec.get_vec(images_list))
+
+        # Close the images once done
+        for image in images_list:
+            image.close()
